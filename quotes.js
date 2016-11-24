f2a415db433684dc460aeeed3d986e6f5b019501
amanda-nCino <amanda.fouts@ncino.com>
2016-11-24 17:23:56 -0500

    Committing first pass of quotes project

---
+++ quotes.js
@@ -0,0 +1,34 @@
+var http = new XMLHttpRequest();
+var url = "https://andruxnet-random-famous-quotes.p.mashape.com/";
+var params = "lorem=ipsum&name=binny";
+var qt;
+var data;
+
+function getQuote() {
+    document.getElementsByClassName("btn").href = "xyz.php";
+    http.open("POST", url, true);
+    http.setRequestHeader("X-Mashape-Key", "LN2cRRkVWKmshmqzmtqXgDecHpycp1sMttKjsneiFQNOn8Msqn");
+    http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    http.setRequestHeader("Accept", "application/json");
+
+    http.onreadystatechange = function() { //Call a function when the state changes.
+        if (http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
+            data = JSON.parse(http.responseText);
+            $('#quote').html(data.quote);
+            $('#author').html(data.author);
+            setQuote(data);
+        }
+    };
+    http.send(params);
+}
+
+function createTwitterButton() {
+    var phrase = data.quote.concat(' - ').concat(data.author);
+    var tweetUrl = 'https://twitter.com/share?text=' +
+      encodeURIComponent(phrase) +
+      '.' +
+      '&url=';
+
+    window.open(tweetUrl);
+}
