c66b5ab41ce4ccd3685c5d208450142840f5639d
esantra <amanda.fouts@gmail.com>
2017-02-16 22:02:26 -0500

    latest updates

--- scratch/scratch18.js
+++ scratch/scratch18.js
@@ -1,14 +1,28 @@
-
-function smallestCommons(arr) {
-  for(var i = 0; i < arr.length; i++){
-    var a = arr[i];
-    var b = arr[i + 1];
-
-
-  }
-  return arr;
+function lcm(a,b) {
+  //least common multiples formula:
+  //lcm(a, b) = a × b / gcd(a, b).
+  //console.log(gcd(a,b));
+  var lcm = (a * b) / gcd(a,b);
+  return lcm;
 }
 
+function gcd(a, b) {
+  if(b === 0){
+    return a;
+  }
+  else {
+    return gcd(b, a % b);
+  }
+}
 
-var a = smallestCommons([1,5]);
-console.log(a);
+function smallestCommons(arr) {
+    arr = arr.sort();
+    var lcmult = 1;
+    var incrementalArr = [];
+    for(var i = arr[0]; i <= arr[arr.length-1]; i++){
+        lcmult = lcm(lcmult, i);
+    }
+    return lcmult;
+}
+
+console.log(smallestCommons([18, 23]));
